9e16f438dd1cfff3791a51e976383cc8d5f517eb
weber5955 <ian.weber595@gmail.com>
2021-07-08 17:26:02 -0400

    First commit for IansProductShop

---
+++ IansProductShop/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using IansProductShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IansProductShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ IansProductShop/Models/Database.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IansProductShop.Models
+{
+    public class Database
+    {
+        public static List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    ProductID = 1,
+                    Name = "Product 1",
+                    Price = (decimal) 499.00
+                },
+                new Product
+                {
+                    ProductID = 2,
+                    Name = "Best product ever",
+                    Price = (decimal) 1109.00
+                },
+                new Product
+                {
+                    ProductID = 3,
+                    Name = "Third product",
+                    Price = (decimal) 358.00
+                },
+                new Product
+                {
+                    ProductID = 4,
+                    Name = "Product of the month",
+                    Price = (decimal) 555.67
+                }
+                ,
+                new Product
+                {
+                    ProductID = 5,
+                    Name = "Fifth product",
+                    Price = (decimal) 245.00
+                }
+                ,
+                new Product
+                {
+                    ProductID = 6,
+                    Name = "Best product ever",
+                    Price = (decimal) 765.00
+                }
+                ,
+                new Product
+                {
+                    ProductID = 7,
+                    Name = "Seventh product",
+                    Price = (decimal) 876.00
+                }
+                ,
+                new Product
+                {
+                    ProductID = 8,
+                    Name = "Eighth product",
+                    Price = (decimal) 234.00
+                }
+                ,
+                new Product
+                {
+                    ProductID = 9,
+                    Name = "Ninth Product",
+                    Price = (decimal) 550.00
+                }
+                ,
+                new Product
+                {
+                    ProductID = 10,
+                    Name = "Tenth product",
+                    Price = (decimal) 654.00
+                }
+            };
+            return products;
+        }
+
+        public static Product GetProduct(string slug)
+        {
+            List<Product> products = Database.GetProducts();
+            foreach(Product p in products)
+            {
+                if (string.Equals(p.Slug,slug))
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
+    }
+}
